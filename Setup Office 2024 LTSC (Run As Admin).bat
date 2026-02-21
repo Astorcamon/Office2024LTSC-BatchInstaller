0e3f7efd6e4fb174703a4622f3cbf2d2a134ff98
Astorcamon <67019001+Astorcamon@users.noreply.github.com>
2026-02-21 02:01:15 +0100

    Update Setup Office 2024 LTSC (Run As Admin).bat
    - Added Language support (Multilingual)

--- Setup Office 2024 LTSC (Run As Admin).bat
+++ Setup Office 2024 LTSC (Run As Admin).bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 mode con: cols=45 lines=40
+powershell -command "& {$h=$host.ui.rawui;$b=$h.buffersize;$w=$h.windowsize;$b.height=999;$h.buffersize=$b}"
 
 :: Force BAT path
 cd /d "%~dp0"
@@ -44,7 +45,7 @@ echo  8. Publisher......... !Publisher!
 echo  9. Project LTSC...... !Project!
 echo 10. Visio LTSC........ !Visio!
 echo.
-echo  S. Install
+echo  I. Install
 echo  X. Exit
 echo.
 set /p opt=Choose an option: 
@@ -65,8 +66,91 @@ if /i "%opt%"=="X" (
     exit /b
 )
 
-if /i not "%opt%"=="S" goto MENU
+if /i not "%opt%"=="I" goto MENU
 
+::Language Select
+cls
+:LANGUAGE
+echo ============================================
+echo   Select the language to install
+echo ============================================
+echo.
+echo  1. English (en-us)
+echo  2. English UK (en-gb)
+echo  3. Spanish (es-es)
+echo  4. Spanish LATAM (es-mx)
+echo  5. French (fr-fr)
+echo  6. German (de-de)
+echo  7. Italian (it-it)
+echo  8. Portuguese BR (pt-br)
+echo  9. Portuguese PT (pt-pt)
+echo 10. Japanese (ja-jp)
+echo 11. Korean (ko-kr)
+echo 12. Chinese Simplified (zh-cn)
+echo 13. Chinese Traditional (zh-tw)
+echo 14. Russian (ru-ru)
+echo 15. Arabic (ar-sa)
+echo 16. Dutch (nl-nl)
+echo 17. Swedish (sv-se)
+echo 18. Norwegian (nb-no)
+echo 19. Danish (da-dk)
+echo 20. Finnish (fi-fi)
+echo 21. Polish (pl-pl)
+echo 22. Czech (cs-cz)
+echo 23. Hungarian (hu-hu)
+echo 24. Turkish (tr-tr)
+echo 25. Greek (el-gr)
+echo 26. Romanian (ro-ro)
+echo 27. Ukrainian (uk-ua)
+echo 28. Indonesian (id-id)
+echo 29. Vietnamese (vi-vn)
+echo 30. Thai (th-th)
+echo 31. Hebrew (he-il)
+echo 32. Hindi (hi-in)
+echo.
+
+set /p lang=Choose an option: 
+
+if "%lang%"=="1"  set "language=en-us"
+if "%lang%"=="2"  set "language=en-gb"
+if "%lang%"=="3"  set "language=es-es"
+if "%lang%"=="4"  set "language=es-mx"
+if "%lang%"=="5"  set "language=fr-fr"
+if "%lang%"=="6"  set "language=de-de"
+if "%lang%"=="7"  set "language=it-it"
+if "%lang%"=="8"  set "language=pt-br"
+if "%lang%"=="9"  set "language=pt-pt"
+if "%lang%"=="10" set "language=ja-jp"
+if "%lang%"=="11" set "language=ko-kr"
+if "%lang%"=="12" set "language=zh-cn"
+if "%lang%"=="13" set "language=zh-tw"
+if "%lang%"=="14" set "language=ru-ru"
+if "%lang%"=="15" set "language=ar-sa"
+if "%lang%"=="16" set "language=nl-nl"
+if "%lang%"=="17" set "language=sv-se"
+if "%lang%"=="18" set "language=nb-no"
+if "%lang%"=="19" set "language=da-dk"
+if "%lang%"=="20" set "language=fi-fi"
+if "%lang%"=="21" set "language=pl-pl"
+if "%lang%"=="22" set "language=cs-cz"
+if "%lang%"=="23" set "language=hu-hu"
+if "%lang%"=="24" set "language=tr-tr"
+if "%lang%"=="25" set "language=el-gr"
+if "%lang%"=="26" set "language=ro-ro"
+if "%lang%"=="27" set "language=uk-ua"
+if "%lang%"=="28" set "language=id-id"
+if "%lang%"=="29" set "language=vi-vn"
+if "%lang%"=="30" set "language=th-th"
+if "%lang%"=="31" set "language=he-il"
+if "%lang%"=="32" set "language=hi-in"
+
+if not defined language (
+	echo.
+	echo Language not found
+	echo.
+	pause
+	goto menu
+)
 
 :: ===== GENERATE XML =====
 echo.
@@ -77,7 +161,7 @@ echo.
 echo ^<Configuration ID="b53935ac-439f-4c47-ab4b-48899f68c2c2"^>
 echo   ^<Add OfficeClientEdition="64" Channel="PerpetualVL2024"^>
 echo     ^<Product ID="ProPlus2024Volume" PIDKEY="XJ2XN-FW8RK-P4HMP-DKDBV-GCVGB"^>
-echo       ^<Language ID="es-es" /^>
+echo       ^<Language ID="%language%" /^>
 
 if %Word%==0        echo       ^<ExcludeApp ID="Word" /^>
 if %Excel%==0       echo       ^<ExcludeApp ID="Excel" /^>
@@ -109,7 +193,6 @@ echo ^</Configuration^>
 echo.
 echo XML generated: config_auto.xml
 echo.
-
 
 :: ===== INSTALL =====
 echo Starting installation...
